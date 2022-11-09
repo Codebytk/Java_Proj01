32c0c30a9c86606d927ae96d026a91932de10604
Codebytk <ltk37733956@gmail.com>
2022-11-09 12:53:13 +0900

    첫번째 커밋

---
+++ src/chapter01/Outprint.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class Outprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//
+		System.out.printf("오늘의 %s는 매우 맑습니다.", "날씨" , "온도는 %d 입니다", 25);
+
+	}
+
+}
